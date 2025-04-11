91aca27337fabbdbc14722a255f8d6f780757a9d
husey <furaknzelik@gmail.com>
2025-04-12 00:53:06 +0200

    deploying 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'; // 👈 switch to HashRouter
 import App from './App.jsx';
 import Trainer from './Trainer.jsx';
 import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/trainer" element={<Trainer />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
